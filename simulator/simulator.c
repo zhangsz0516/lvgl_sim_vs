6583b3f0629dbe8cfcaf119435e7cdb5101aa466
张世争 <zhangsz0516@163.com>
2025-11-09 10:54:21 +0800

    init simulator project

---
+++ simulator/simulator.c
@@ -0,0 +1,108 @@
+﻿#include <Windows.h>
+
+//#include <LvglWindowsIconResource.h>
+
+#include "lvgl/lvgl.h"
+#include "lvgl/examples/lv_examples.h"
+#include "lvgl/demos/lv_demos.h"
+
+/**
+ * Basic example to create a "Hello world" label
+ */
+void lv_example_get_started_1(void)
+{
+    /*Change the active screen's background color*/
+    lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0x003a57), LV_PART_MAIN);
+
+    /*Create a white label, set its text and align it to the center*/
+    lv_obj_t* label = lv_label_create(lv_screen_active());
+    lv_label_set_text(label, "Hello world");
+    lv_obj_set_style_text_color(lv_screen_active(), lv_color_hex(0xffffff), LV_PART_MAIN);
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+}
+
+int main()
+{
+    lv_init();
+
+    /*
+     * Optional workaround for users who wants UTF-8 console output.
+     * If you don't want that behavior can comment them out.
+     *
+     * Suggested by jinsc123654.
+     */
+#if LV_TXT_ENC == LV_TXT_ENC_UTF8
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+#endif
+
+    int32_t zoom_level = 100;
+    bool allow_dpi_override = false;
+    bool simulator_mode = true;
+    lv_display_t* display = lv_windows_create_display(
+        L"LVGL Windows Simulator Display 1",
+        800,
+        480,
+        zoom_level,
+        allow_dpi_override,
+        simulator_mode);
+    if (!display)
+    {
+        return -1;
+    }
+
+    HWND window_handle = lv_windows_get_display_window_handle(display);
+    if (!window_handle)
+    {
+        return -1;
+    }
+
+#if 0
+    HICON icon_handle = LoadIconW(
+        GetModuleHandleW(NULL),
+        MAKEINTRESOURCE(IDI_LVGL_WINDOWS));
+    if (icon_handle)
+    {
+        SendMessageW(
+            window_handle,
+            WM_SETICON,
+            TRUE,
+            (LPARAM)icon_handle);
+        SendMessageW(
+            window_handle,
+            WM_SETICON,
+            FALSE,
+            (LPARAM)icon_handle);
+    }
+#endif
+
+    lv_indev_t* pointer_indev = lv_windows_acquire_pointer_indev(display);
+    if (!pointer_indev)
+    {
+        return -1;
+    }
+
+    lv_indev_t* keypad_indev = lv_windows_acquire_keypad_indev(display);
+    if (!keypad_indev)
+    {
+        return -1;
+    }
+
+    lv_indev_t* encoder_indev = lv_windows_acquire_encoder_indev(display);
+    if (!encoder_indev)
+    {
+        return -1;
+    }
+
+    //lv_demo_widgets();
+    //lv_demo_benchmark();
+    lv_example_get_started_1();
+
+    while (1)
+    {
+        uint32_t time_till_next = lv_timer_handler();
+        lv_delay_ms(time_till_next);
+    }
+
+    return 0;
+}
